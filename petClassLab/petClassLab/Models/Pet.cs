eb2500798988bf6a808b8275b224f70f5135864a
MusawerWaziri <100398168+MusawerWaziri@users.noreply.github.com>
2022-02-26 13:27:25 -0500

    Add files via upload

---
+++ petClassLab/petClassLab/Models/Pet.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace petClassLab.Models
+{
+    class Pet
+    {
+        public string Name { get;}
+        public string Owner { get; private set; }
+        public int Age { get;}
+        public string Description { get;}
+        public bool IsHouseTrained { get; private set; }
+
+        public Pet(string name, int age, string description)
+        {
+            Name = name;
+            Age = age;
+            Description = description;
+            Owner = "no one";
+            IsHouseTrained = false;
+        }
+        public override string ToString()
+        {
+            return $"Name: {Name}\n" +
+                   $"Age: {Age}\n" +
+                   $"About: {Description}\n" +
+                   $"Owner: {Owner}\n" +
+                   $"Trained: {IsHouseTrained}\n";
+        }
+        public void SetOwner(string newOwner)
+        {
+            Owner = newOwner;
+        }
+        public void Train()
+        {
+            IsHouseTrained = true;
+        }
+    }
+}
---
+++ petClassLab/petClassLab/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using petClassLab.Models;
+using System.Collections.Generic;
+
+namespace petClassLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pet cat = new Pet("Tom",12, "Hates Jerry");
+            Pet dog = new Pet("Scooby", 7, "Cool");
+            Pet cat2 = new Pet("Garfield", 10, "Loves lasagna");
+            Pet bird = new Pet("Tweety", 3, "Very small");
+
+            List<Pet> pets = new List<Pet>();
+            pets.Add(cat);
+            pets.Add(dog);
+            pets.Add(cat2);
+            pets.Add(bird);
+
+            cat.SetOwner("Bob");
+            cat.Train();
+            dog.Train();
+            cat2.SetOwner("Jim");
+
+            Console.WriteLine("All Pets");
+            foreach(object Pet in pets)
+            {
+                Console.WriteLine($"{Pet}");
+            }
+
+            Console.Write("Enter an owner's name: ");
+            string name = Console.ReadLine();
+            foreach(Pet pet in pets)
+            {
+                if (pet.Owner == name)
+                {
+                    Console.WriteLine("They own the following: ");
+                    Console.WriteLine($"{pet}");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("They are not an owner");
+                    break;
+                }
+            }
+        }
+    }
+}
